377cfaa29758ca57a07318b6cd27323202d20619
Tene Tiitma <75568825+tenetiitma@users.noreply.github.com>
2022-01-20 12:30:15 +0200

    Update dbconfig.php

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -10,7 +10,7 @@ class Dbconfig {
     }
         
     private function db_connect() {
-        $this->connection = mysqli_connect('d108586.mysql.zonevs.eu', 'd108586sa414891', 'sjv87HnFS9gB23Ld5U', 'd108586sd458550');
+        $this->connection = mysqli_connect('', '', '', '');
         if(mysqli_connect_error()) {
             die("Connection failed!");
         }
